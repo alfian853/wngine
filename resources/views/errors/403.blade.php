1516fa9eea4244ff7f8a6e3cd8ae06637bc4dec2
alcredo <simanjuntakalcredo@gmail.com>
2018-05-29 06:33:36 +0700

    403 and 404

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,1 +1,55 @@
-403 BOSS FORBIDDEN
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Ropa+Sans" rel="stylesheet">
+    <style>
+    body
+    {
+    padding-top:50px;
+    font-family: 'Ropa Sans', sans-serif;
+    margin-top: 30px;
+    background-color: #F0CA00;
+    background-color: grey;
+    text-align: center;
+    color: #fff;
+    }
+    .error-heading
+    {
+    margin: 50px auto;
+    width: 250px;
+    border: 5px solid #fff;
+    font-size: 126px;
+    line-height: 126px;
+    border-radius: 30px;
+    text-shadow: 6px 6px 5px #000;
+    }
+    .error-heading img
+    {
+        width: 100%;
+    }
+    .error-main h1
+    {
+    font-size: 120px;
+    margin: 0px;
+    color: white;
+    text-shadow: 0px 0px 5px #fff;
+    }
+    button
+    {
+        color:white;
+        background:grey;
+        font-size:15px;
+        font-weight:bold;
+    }
+    </style>
+    
+</head>
+<body>
+	<div class="error-main">
+		<h1>Oops!</h1>
+		<div class="error-heading">403</div>
+		<p style="font-size:25px;">You do not have permission to access the document or program that you requested.</p>
+        <button>Go to Home</button>
+	</div>
+</body>
+</html>
