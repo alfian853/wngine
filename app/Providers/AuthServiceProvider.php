c3559a026af1caee4cbe29ae28da17bd3f881572
rahadiankp <rahadian.k.putra@gmail.com>
2018-05-28 22:18:59 +0700

    Company policy for writing company's testimony

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,6 +15,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
         'App\Job' => 'App\Policies\JobPolicy',
+        'App\Company' => 'App\Policies\CompanyPolicy',
     ];
 
     /**
