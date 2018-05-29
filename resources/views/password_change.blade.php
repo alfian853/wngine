25acabad4abd7eeab05e260fc7a8121203d193f5
alcredo <simanjuntakalcredo@gmail.com>
2018-05-30 01:40:44 +0700

    change password ui

---
+++ resources/views/password_change.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.master')
+
+@section('title', 'Reset Password')
+
+@section('add-script')
+  @parent
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/reset.css') }}">
+@endsection
+
+@section('content')
+<div class="container">
+        <div class="col-md-12 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('post.member.password.reset') }}" role="form">
+                        {{ csrf_field() }}
+			                      <h2>Reset Password</h2><hr>
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}" >
+                            <label for="password_old" style="font-weigth:bold" class="col-md-12 control-label">Old password</label>
+                            <div class="col-md-12">
+                                <input id="password_old"  type="password" class="form-control" name="password_1" required>
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <label for="password_1" style="font-weigth:bold;margin-top:15px;" class="col-md-12 control-label">New password</label>
+                            <div class="col-md-12">
+                                <input id="password_1" type="password" class="form-control" name="password_1" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <label for="email" style="font-weigth:bold;margin-top:15px;" class="col-md-12 control-label">Retype new password</label>
+                            <div class="col-md-12">
+                                <input id="password_2" type="password" class="form-control" name="password_2" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <input type="hidden" name="email" value="@php echo $_GET['email']; @endphp"/>
+
+                        <div class="form-group">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-primary">
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+        </div>
+    </div>
+</div>
+</div>
+@endsection
